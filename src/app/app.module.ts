830341bdab56704dc198c4b0ca2e295c4d0de084
Nuno Leite de Faria <nleitefaria@gmail.com>
2018-04-30 15:50:47 +0100

    Added Companies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { NgxPaginationModule } from 'ngx-pagination';
 //import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -23,6 +23,7 @@ import { CompaniesComponent } from './components/companies/companies.component';
     HttpModule,
     NgxPaginationModule,  
     FormsModule,
+    ReactiveFormsModule,
     //NgbModule.forRoot()
   ],
   providers: [],
