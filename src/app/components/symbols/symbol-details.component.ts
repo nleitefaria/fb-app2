782990e74f06a76c5f172e0fdf7d7dc82fa4dd5d
nleitefaria <nleitefaria@gmail.com>
2018-09-14 22:01:32 +0100

    Added Key Stats

--- src/app/components/symbols/symbol-details.component.ts
+++ src/app/components/symbols/symbol-details.component.ts
@@ -1,153 +1,167 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { SymbolsService } from '../../services/symbols.service';
-import { CompaniesService } from '../../services/companies.service';
-import { EarningsService } from '../../services/earnings.service';
-import { EffectiveSpreadService } from '../../services/effective-spread.service';
-import { FinancialsService } from '../../services/financials.service';
-import { LogosService } from '../../services/logos.service';
-import { BooksService } from '../../services/books.service';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {SymbolsService} from '../../services/symbols.service';
+import {CompaniesService} from '../../services/companies.service';
+import {EarningsService} from '../../services/earnings.service';
+import {EffectiveSpreadService} from '../../services/effective-spread.service';
+import {FinancialsService} from '../../services/financials.service';
+import {LogosService} from '../../services/logos.service';
+import {BooksService} from '../../services/books.service';
+import {StatsService} from '../../services/stats.service';
 
 @Component({
   selector: 'app-symbol-details',
   templateUrl: './symbol-details.component.html',
   styleUrls: ['./symbol-details.component.css'],
-  providers: [SymbolsService, CompaniesService, EarningsService, EffectiveSpreadService, FinancialsService, LogosService, BooksService]
+  providers: [SymbolsService, CompaniesService, EarningsService, EffectiveSpreadService, FinancialsService, LogosService, BooksService, StatsService]
 })
 
-export class SymbolDetailsComponent implements OnInit 
-{
-	id: string;
-	logo : any;
-	location: string = 'symbols';	
-	symbols : any;
-	symbolsLoading : boolean;
-	company : any;
-	earnings : any[];
-	effectiveSpreads : any[];
-	financials: any[];
-	book : any;
-	trades : any[]; 
-	ipo : any;
-	
-  constructor(private route: ActivatedRoute, private httpService : SymbolsService, private httpService1 : CompaniesService, private httpService2 : EarningsService, private httpService3 : EffectiveSpreadService, private httpService4 : FinancialsService, private httpService5 : LogosService, private httpService6 : BooksService) 
-  	{
-  		this.route.params.subscribe((params) => 
-  		{
-  			this.id = params.id; 				
-		});	
-  	}
+export class SymbolDetailsComponent implements OnInit {
+  id: string;
+  logo: any;
+  location: string = 'symbols';
+  symbols: any;
+  symbolsLoading: boolean;
+  company: any;
+  earnings: any[];
+  effectiveSpreads: any[];
+  financials: any[];
+  book: any;
+  trades: any[];
+  ipo: any;
+  stats: any;
 
-  	ngOnInit()
-    {	
-  		this.init(); 		 		
-  	}
-  	
-  	init()
-  	{
-  		this.getCompany(this.id);
-      	this.getEarnings(this.id);
-      	this.getEffectiveSpreads(this.id);
-      	this.getFinancials(this.id);	
-      	this.getLogo(this.id);
-      	this.getBook(this.id);
-  	}
-  	
-  	getCompany(sym : string)
-    {
-    	this.httpService1.getCompany(sym).subscribe(
-			response =>{
-				if(response.error) { 
-					alert('Server Error');
-				} else {																																
-					this.company = response;															
-				}
-			},
-			error =>{
-				alert('Server error');
-			}
-		);
-    }
-    
-    getEarnings(sym : string)
-    {
-    	this.httpService2.getEarnings(sym).subscribe(
-			response =>{
-				if(response.error) { 
-					alert('Server Error');
-				} else {																																
-					this.earnings = response.earnings;														
-				}
-			},
-			error =>{
-				alert('Server error');
-			}
-		);
-    }
-    
-    getEffectiveSpreads(sym : string)
-    {
-    	this.httpService3.getEffectiveSpreads(sym).subscribe(
-			response =>{
-				if(response.error) { 
-					alert('Server Error');
-				} else {																																
-					this.effectiveSpreads = response;												
-				}
-			},
-			error =>{
-				alert('Server error');
-			}
-		);
-    }
-    
-    getFinancials(sym : string)
-    {
-    	this.httpService4.getFinancials(sym).subscribe(
-			response =>{
-				if(response.error) { 
-					alert('Server Error');
-				} else {																																
-					this.financials = response.financials;												
-				}
-			},
-			error =>{
-				alert('Server error');
-			}
-		);
-    }
-    
-    getLogo(sym : string)
-    {
-    	this.httpService5.getLogo(sym).subscribe(
-			response =>{
-				if(response.error) { 
-					alert('Server Error');
-				} else {																																
-					this.logo = response.url;												
-				}
-			},
-			error =>{
-				alert('Server error');
-			}
-		);
-    }
-    
-    getBook(sym : string)
-    {
-    	this.httpService6.getBook(sym).subscribe(
-			response =>{
-				if(response.error) { 
-					alert('Server Error');
-				} else {
-																																			
-					this.book = response;
-					this.trades = this.book.trades;														
-				}
-			},
-			error =>{
-				alert('Server error');
-			}
-		);
-    }
+  constructor(private route: ActivatedRoute, private httpService: SymbolsService, private httpService1: CompaniesService, private httpService2: EarningsService, private httpService3: EffectiveSpreadService, private httpService4: FinancialsService, private httpService5: LogosService, private httpService6: BooksService, private httpService7: StatsService) {
+    this.route.params.subscribe((params) => {
+      this.id = params.id;
+    });
+  }
+
+  ngOnInit() {
+    this.init();
+  }
+
+  init() {
+    this.getCompany(this.id);
+    this.getEarnings(this.id);
+    this.getEffectiveSpreads(this.id);
+    this.getFinancials(this.id);
+    this.getLogo(this.id);
+    this.getBook(this.id);
+    this.getStats(this.id);
+  }
+
+  getCompany(sym: string) {
+    this.httpService1.getCompany(sym).subscribe(
+      response => {
+        if (response.error) {
+          alert('Server Error');
+        } else {
+          this.company = response;
+        }
+      },
+      error => {
+        alert('Server error');
+      }
+    );
+  }
+
+  getEarnings(sym: string) {
+    this.httpService2.getEarnings(sym).subscribe(
+      response => {
+        if (response.error) {
+          alert('Server Error');
+        } else {
+          this.earnings = response.earnings;
+        }
+      },
+      error => {
+        alert('Server error');
+      }
+    );
+  }
+
+  getEffectiveSpreads(sym: string) {
+    this.httpService3.getEffectiveSpreads(sym).subscribe(
+      response => {
+        if (response.error) {
+          alert('Server Error');
+        } else {
+          this.effectiveSpreads = response;
+        }
+      },
+      error => {
+        alert('Server error');
+      }
+    );
+  }
+
+  getFinancials(sym: string) {
+    this.httpService4.getFinancials(sym).subscribe(
+      response => {
+        if (response.error) {
+          alert('Server Error');
+        } else {
+          this.financials = response.financials;
+        }
+      },
+      error => {
+        alert('Server error');
+      }
+    );
+  }
+
+  getLogo(sym: string) {
+    this.httpService5.getLogo(sym).subscribe(
+      response => {
+        if (response.error) {
+          alert('Server Error');
+        } else {
+          this.logo = response.url;
+        }
+      },
+      error => {
+        alert('Server error');
+      }
+    );
+  }
+
+  getBook(sym: string) {
+    this.httpService6.getBook(sym).subscribe(
+      response => {
+        if (response.error) {
+          alert('Server Error');
+        } else {
+
+          this.book = response;
+          this.trades = this.book.trades;
+        }
+      },
+      error => {
+        alert('Server error');
+      }
+    );
+  }
+
+
+  getStats(sym: string) {
+    this.httpService7.getStats(sym).subscribe(
+      response => {
+        if (response.error) {
+          alert('Server Error');
+        }
+        else {
+          this.stats = response;
+          console.log(this.stats);
+        }
+      },
+      error => {
+        alert('Server error');
+      }
+    );
+  }
+
+
+
+
 }
---
+++ src/app/services/stats.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class StatsService 
+{
+    private BASE_URL:string = environment.apiUrl;
+
+    constructor(private http:Http) 
+    { 
+    }
+    
+    public getStats(symb: string):any
+    {   
+      return this.http.get(`${this.BASE_URL}stock/${symb}/stats`)
+        .map((response:Response) => response.json())
+        .catch((error:any) => Observable.throw(error.json().error) || 'Server Error');
+    }
+}
