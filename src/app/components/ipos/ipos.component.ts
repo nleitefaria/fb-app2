24a170b1cb04c50ee2df1c142c09f952ec3c1a7c
nleitefaria <nleitefaria@gmail.com>
2018-09-15 20:26:38 +0100

    Added IPOs view

--- src/app/components/ipos/ipos.component.ts
+++ src/app/components/ipos/ipos.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { IposService } from '../../services/ipos.service';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {IposService} from '../../services/ipos.service';
 
 @Component({
   selector: 'app-ipos',
@@ -11,45 +11,40 @@ import { IposService } from '../../services/ipos.service';
 
 export class IposComponent implements OnInit 
 {
-	id: string;
-	ipo : any;
-	location: string = 'ipos';	
+  id: string;
+  ipo: any;
+  location: string = 'IPOs';
 
-  	constructor(private route: ActivatedRoute, private httpService : IposService) 
-  	{ 
-  		this.route.params.subscribe((params) => 
-  		{
-  			this.id = params.id; 				
-		});	
-  	}
+  constructor(private route: ActivatedRoute, private httpService: IposService) {
+    this.route.params.subscribe((params) => {
+      this.id = params.id;
+    });
+  }
 
-  	ngOnInit()
-    {	
-  		this.init(); 		 		
-  	}
-  	
-  	init()
-  	{
-      	this.getUpcomingIpos();
-  	}
-  	
-  	getUpcomingIpos()
-    {
-    	this.httpService.getUpcomingIpos().subscribe(
-			response =>{
-				if(response.error) { 
-					alert('Server Error');
-				} else {
-																																			
-					this.ipo = response;
-					//alert(this.ipo);									
-				}
-			},
-			error =>{
-				alert('Server error');
-			}
-		);
-    
-    }
+  ngOnInit() {
+    this.init();
+  }
+
+  init() {
+    this.getUpcomingIpos();
+  }
+
+  getUpcomingIpos() {
+    this.httpService.getUpcomingIpos().subscribe(
+      response => {
+        if (response.error) {
+          alert('Server Error');
+        } else {
+
+          this.ipo = response;
+          //alert(this.ipo);									
+        }
+      },
+      error => {
+        alert('Server error');
+      }
+    );
+
+  }
 
 }
