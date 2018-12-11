c03be23fb3b86301697bc97294fb4cf354e0f6b5
nleitefaria <nleitefaria@gmail.com>
2018-12-11 19:36:25 +0000

    Updated losers

--- src/app/components/losers/losers.component.ts
+++ src/app/components/losers/losers.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { LosersService } from '../../services/losers.service';
 
 @Component({
   selector: 'app-losers',
   templateUrl: './losers.component.html',
-  styleUrls: ['./losers.component.css']
+  styleUrls: ['./losers.component.css'],
+  providers: [LosersService]
 })
 export class LosersComponent implements OnInit {
 
-  constructor() { }
+    losers : any[];
+    location: string = 'Losers';
 
-  ngOnInit() {
-  }
+    constructor(private httpService: LosersService) 
+    {       
+    }
+
+    ngOnInit()
+    {
+        this.init();
+    }
+
+    init() 
+    {
+        this.getLosers();
+    }
+
+    getLosers() 
+    {
+        this.httpService.getLosers().subscribe(                
+            response => {
+              if (response.error) 
+              {
+                alert('Server Error');
+              } 
+              else 
+              {
+                this.losers = response;                     
+              }
+            },
+            error => {
+              alert('Server error');
+            }
+            
+          );       
+    }
 
 }
