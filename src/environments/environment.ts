ec16d5ca13652ed0c7d84e479b45b8d8e1e7fe07
nleitefaria <nleitefaria@gmail.com>
2019-06-08 13:56:19 +0100

    clean

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: "https://cloud.iexapis.com/stable/",
-  tkn: "sk_1e848d08c63a4468b3032d72ed4bb740"
+  tkn: "pk_cdc794662cb642ec9311feaee8ef47bc"
 };
