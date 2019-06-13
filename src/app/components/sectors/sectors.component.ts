70ddc398afea1f259e2dc53974f08fc8d2a2ff4b
User01 <User01@Windows10base>
2019-06-13 14:46:36 +0100

    Added sectors

--- src/app/components/sectors/sectors.component.ts
+++ src/app/components/sectors/sectors.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { SectorsService } from '../../services/sectors.service';
 
 @Component({
   selector: 'app-sectors',
   templateUrl: './sectors.component.html',
-  styleUrls: ['./sectors.component.css']
+  styleUrls: ['./sectors.component.css'],
+  providers: [SectorsService]
 })
 export class SectorsComponent implements OnInit {
 
-  constructor() { }
+	location: string = 'Sectors';
+    sectors: any;
 
-  ngOnInit() {
-  }
+  	constructor(private httpService : SectorsService) 
+  	{ 
+  	}
+
+  	ngOnInit() 
+  	{ 	
+  		this.init(); 	 	
+  	}
+  	
+  	init()
+    {  
+        this.httpService.getSectors().subscribe(
+			response =>{
+				if(response.error) { 
+					alert('Server Error');
+				} else {																																
+					this.sectors = response;															
+				}
+			},
+			error =>{
+				alert('Server error');
+			}
+		);
+    }
+	
 
 }
